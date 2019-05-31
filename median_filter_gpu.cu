54339f0081073dbc17274f0a162336bae384f984
NieGrzegorz <grzegorz0711@gmail.com>
2019-05-31 19:50:26 +0200

    gpu median fix

--- median_filter_gpu.cu
+++ median_filter_gpu.cu
@@ -4,7 +4,8 @@ extern "C"{
 }
 BMPFile* bmp_img; 
 unsigned char* host_img_in_data;
-unsigned char* host_img_out_data;  
+unsigned char* host_img_out_data; 
+unsigned char* host_test_data; 
 int blockSize;
 
 __device__ void _getMask(unsigned char* mask, unsigned char* image_data, int width, int x, int y)
@@ -34,36 +35,47 @@ __device__ void _sort(unsigned char* mask){
 
 void prologue()
 {
+	host_test_data = (unsigned char*)malloc(sizeof(bmp_img->data)); 
 	cudaMalloc((void**)&host_img_in_data, sizeof(bmp_img->data)); 
 	cudaMemcpy(host_img_in_data, bmp_img->data, sizeof(bmp_img->data), cudaMemcpyHostToDevice);	
 	
 	cudaMalloc((void**)&host_img_out_data, sizeof(bmp_img->data)); 
-	cudaMemcpy(host_img_out_data, bmp_img->data, sizeof(bmp_img->data), cudaMemcpyHostToDevice);	
+	//cudaMemcpy(host_img_out_data, bmp_img->data, sizeof(bmp_img->data), cudaMemcpyHostToDevice);	
 }
 
 void epilogue()
 {
-	cudaMemcpy(bmp_img->data, host_img_out_data, sizeof(bmp_img->data), cudaMemcpyDeviceToHost); 
+	//cudaMemcpy(bmp_img->data, host_img_out_data, sizeof(bmp_img->data), cudaMemcpyDeviceToHost); 
+	cudaMemcpy(host_test_data, host_img_out_data, sizeof(bmp_img->data), cudaMemcpyDeviceToHost); 
 	cudaFree(host_img_out_data);
 	cudaFree(host_img_in_data);
 }
 
-__global__ void _applyFilter(unsigned char* host_img_data, unsigned char* res, int width, int height)
+void _printPixelVals(unsigned char* image_data, int width, int height)
 {
-	int i = threadIdx.x; 
-	int j = threadIdx.y; 
+	for(int i = 0; i < height; i++){
+		for(int j = 0; j < width; j++){
+			printf("Img(%d, %d): %02x\n", i, j, image_data[i * height  +j]);
+		}
+	}
+}
+
+__global__ void _applyFilter(unsigned char* host_img_in_data, unsigned char* host_img_out_data, int width, int height)
+{
+	int i = blockIdx.x * blockDim.x +threadIdx.x; 
+	int j = blockIdx.y * blockDim.y + threadIdx.y; 
 
 	unsigned char mask[9]; 
 	
 	if((i != 0) && (j != 0) && (i != width - 1) && (j != height - 1))
 	{
-		_getMask(mask, host_img_data, width, i, j);
+		_getMask(mask, host_img_in_data, width, i, j);
 		_sort(mask);
-		res[(i-1) * width + (j -1)] = mask[4]; 	
+		host_img_out_data[i * width + j] = mask[4]; 	
 	}
 	else 
 	{
-		res[j * width + i] =  0; 
+		host_img_out_data[j * width + i] =  0; 
 	}
 }
 
@@ -92,8 +104,11 @@ int main(int argc, char **argv)
 	prologue(); 
 	_applyFilter<<<1, threadsPerBlock>>>(host_img_in_data, host_img_out_data, bmp_img->header.width_px, bmp_img->header.height_px);	
 	cudaThreadSynchronize();
-	epilogue(); 
+	
 
+	epilogue(); 
+	//_printPixelVals(bmp_img->data, bmp_img->header.width_px, bmp_img->header.height_px); 
+	bmp_img->data = host_test_data; 
 	write_bmp(bmp_img, fOut); 
 	fclose(fOut); 
 	free_bmp(bmp_img); 
