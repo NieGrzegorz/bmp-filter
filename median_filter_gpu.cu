e9be4a8049a2df246f906eb6bd3d2e28043209c1
NieGrzegorz <grzegorz0711@gmail.com>
2019-06-01 10:16:08 +0200

    fixo

--- median_filter_gpu.cu
+++ median_filter_gpu.cu
@@ -1,12 +1,16 @@
 #include <stdio.h>
+
 extern "C"{
 #include "bmp.h"
 }
+
 BMPFile* bmp_img; 
 unsigned char* host_img_in_data;
 unsigned char* host_img_out_data; 
-unsigned char* host_test_data; 
+unsigned char* host_test_data;
+unsigned char* host_test_data2;  
 int blockSize;
+int size = 1024 * 1024 * sizeof(char); 
 
 __device__ void _getMask(unsigned char* mask, unsigned char* image_data, int width, int x, int y)
 {
@@ -35,18 +39,34 @@ __device__ void _sort(unsigned char* mask){
 
 void prologue()
 {
-	host_test_data = (unsigned char*)malloc(sizeof(bmp_img->data)); 
-	cudaMalloc((void**)&host_img_in_data, sizeof(bmp_img->data)); 
-	cudaMemcpy(host_img_in_data, bmp_img->data, sizeof(bmp_img->data), cudaMemcpyHostToDevice);	
+	host_test_data = (unsigned char*)malloc(size); 
+	host_test_data2 = (unsigned char*)malloc(size);
+	host_test_data = bmp_img->data;  
+	cudaMalloc((void**)&host_img_in_data, size); 
+	cudaMemcpy(host_img_in_data, host_test_data,size, cudaMemcpyHostToDevice);	
 	
-	cudaMalloc((void**)&host_img_out_data, sizeof(bmp_img->data)); 
-	//cudaMemcpy(host_img_out_data, bmp_img->data, sizeof(bmp_img->data), cudaMemcpyHostToDevice);	
+	cudaError_t err; 
+	cudaMalloc((void**)&host_img_out_data, size); 
+	cudaMemcpy(host_img_out_data, bmp_img->data, size, cudaMemcpyHostToDevice);	
+	err = cudaGetLastError();
+	if (cudaSuccess != err)
+	{
+		printf("Prologue failed\n"); 
+		exit(1);
+	} 	
 }
 
 void epilogue()
 {
-	//cudaMemcpy(bmp_img->data, host_img_out_data, sizeof(bmp_img->data), cudaMemcpyDeviceToHost); 
-	cudaMemcpy(host_test_data, host_img_out_data, sizeof(bmp_img->data), cudaMemcpyDeviceToHost); 
+	cudaMemcpy(host_test_data2, host_img_in_data, size, cudaMemcpyDeviceToHost); 
+	cudaMemcpy(host_test_data, host_img_out_data, size, cudaMemcpyDeviceToHost); 
+	cudaError_t err; 
+	err = cudaGetLastError();
+	if (cudaSuccess != err)
+	{
+		printf("Epilogue failed\n"); 
+		exit(1);
+	} 	
 	cudaFree(host_img_out_data);
 	cudaFree(host_img_in_data);
 }
@@ -62,20 +82,20 @@ void _printPixelVals(unsigned char* image_data, int width, int height)
 
 __global__ void _applyFilter(unsigned char* host_img_in_data, unsigned char* host_img_out_data, int width, int height)
 {
-	int i = blockIdx.x * blockDim.x +threadIdx.x; 
-	int j = blockIdx.y * blockDim.y + threadIdx.y; 
+	int i = blockIdx.y * blockDim.y + threadIdx.y; 
+	int j = blockIdx.x * blockDim.x + threadIdx.x; 
 
 	unsigned char mask[9]; 
 	
-	if((i != 0) && (j != 0) && (i != width - 1) && (j != height - 1))
+	if((i == 0) || (j == 0) || (i == width - 1) || (j == height - 1))
+	{
+		host_img_out_data[j * width + i] =  0; 
+	}
+	else 
 	{
 		_getMask(mask, host_img_in_data, width, i, j);
 		_sort(mask);
 		host_img_out_data[i * width + j] = mask[4]; 	
-	}
-	else 
-	{
-		host_img_out_data[j * width + i] =  0; 
 	}
 }
 
@@ -100,14 +120,15 @@ int main(int argc, char **argv)
 	
 	fOut = fopen("out_gpu.bmp", "wb");
 
-	dim3 threadsPerBlock(1024, 1024); 
+	dim3 threadsPerBlock(32, 32);
+	dim3 dimGrid((int)ceil((float)bmp_img->header.width_px / (float)32),(int)ceil((float)bmp_img->header.height_px / (float)32)); 
 	prologue(); 
-	_applyFilter<<<1, threadsPerBlock>>>(host_img_in_data, host_img_out_data, bmp_img->header.width_px, bmp_img->header.height_px);	
+	_applyFilter<<<dimGrid, threadsPerBlock>>>(host_img_in_data, host_img_out_data, bmp_img->header.width_px, bmp_img->header.height_px);	
 	cudaThreadSynchronize();
 	
 
 	epilogue(); 
-	//_printPixelVals(bmp_img->data, bmp_img->header.width_px, bmp_img->header.height_px); 
+	_printPixelVals(bmp_img->data, bmp_img->header.width_px, bmp_img->header.height_px); 
 	bmp_img->data = host_test_data; 
 	write_bmp(bmp_img, fOut); 
 	fclose(fOut); 
